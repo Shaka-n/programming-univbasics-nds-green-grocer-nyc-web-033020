e73663b7e38e7f589303ca68ac7edf622c53d8ad
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-27 19:12:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -58,7 +58,8 @@ end
 def apply_clearance(cart)
   index = 0 
   while index < cart.length do 
-    if cart[index]
+    if cart[index][:clearance]
+      cart[index]
 end
 
 def checkout(cart, coupons)
