2427c00be1bfb5e45d01338630a9d13621d61d62
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-27 19:12:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -56,9 +56,9 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  index = 0 
+  while index < cart.length do 
+    
 end
 
 def checkout(cart, coupons)
