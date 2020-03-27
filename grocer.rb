17e9973c4fb967f73425d2cacf25c8772fc12287
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-27 20:08:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def consolidate_cart(cart)
         :item => cart[index][:item]
         :price => cart[index][:price]
         :clearance => cart[index][:clearance]
-        :count
+        :count => 1
       }
     end 
   end
